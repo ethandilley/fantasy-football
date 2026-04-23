f52676411bdf92045169ff5d7257eeb42f72ec83
ethandilley <ethandilley68@gmail.com>
2026-04-23 03:23:02 +0000

    stash

--- service/src/service/routers/kaggle_router.py
+++ service/src/service/routers/kaggle_router.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter, Depends
+import csv
+from io import StringIO
 from typing import Annotated
-from common.db import get_clickhouse_client
+
 from clickhouse_connect.driver.client import Client
+from common.db import get_clickhouse_client
+from fastapi import APIRouter, Depends, Response
 
 router = APIRouter()
 
@@ -9,11 +12,54 @@ ClickhouseClient = Annotated[Client, Depends(get_clickhouse_client)]
 
 
 @router.get("/data", tags=["Kaggle"])
-async def data(client: ClickhouseClient, year: int = 1999):
-    result = client.query(f"SELECT * FROM gold.playergame where season = {year}")
-    
+async def data(
+    client: ClickhouseClient,
+    start_year: int = 2025,
+    end_year: int = 2025,
+    limit: int = 0,
+):
+    query = f"SELECT * FROM gold.playergame WHERE season >= {start_year} and season <= {end_year}"
+    if limit != 0:
+        query = query + f" limit {limit}"
+
+    result = client.query(query)
+
     columns = result.column_names
     rows = result.result_rows
 
     data = [dict(zip(columns, row)) for row in rows]
     return data
+
+
+@router.get("/download", tags=["Kaggle"])
+async def download(
+    client: ClickhouseClient,
+    start_year: int = 2025,
+    end_year: int = 2025,
+    limit: int = 0,
+):
+    query = f"SELECT * FROM gold.playergame WHERE season >= {start_year} and season <= {end_year}"
+    if limit != 0:
+        query = query + f" limit {limit}"
+
+    result = client.query(query)
+
+    columns = result.column_names
+    rows = result.result_rows
+
+    buffer = StringIO()
+    writer = csv.writer(buffer)
+
+    writer.writerow(columns)
+    writer.writerows(rows)
+
+    csv_content = buffer.getvalue()
+    buffer.close()
+
+    return Response(
+        content=csv_content,
+        media_type="text/csv",
+        headers={
+            "Content-Disposition": f'attachment; filename="playergame_{start_year}_{end_year}.csv"',
+        },
+    )
