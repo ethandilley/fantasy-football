a29687085bd37eea002e3222b87b41d669198199
ethandilley <149187803+ethandilley@users.noreply.github.com>
2026-04-27 05:04:41 -0400

    Add Basic Kaggle Endpoints (#4)

--- airflow/dags/gold/player_games.py
+++ airflow/dags/gold/player_games.py
@@ -17,6 +17,7 @@ def gold_player_games():
     @task
     def load():
         clickhouse_client = ClickhouseClient()
+        print(INSERT_GOLD)
         values = clickhouse_client.client.command(INSERT_GOLD)
         print(values)
 
@@ -27,6 +28,32 @@ def gold_player_games():
         print(df)
 
 
+        # def calculate_fantasy_points(row):
+        #     passing_yards = row["passing_yards"]
+        #     passing_tds = row["passing_tds"]
+        #     interceptions = row["interceptions"]
+        #     rushing_yards = row["rushing_yards"]
+        #     rushing_tds = row["rushing_tds"]
+        #     receptions = row["receptions"]
+        #     receiving_yards = row["receiving_yards"]
+        #     receiving_tds = row["receiving_tds"]
+        #     fumbles_lost = row["fumbles_lost"]
+        #
+        #     fantasy_points = (
+        #         6 * (passing_tds + rushing_tds + receiving_tds)
+        #         + 0.1 * (rushing_yards + receiving_yards)
+        #         + 0.04 * (passing_yards)
+        #         + 1 * receptions
+        #         + -2 * (interceptions + fumbles_lost)
+        #     )
+        #     return fantasy_points
+        # df["fantasy_points"] = df.apply(calculate_fantasy_points, axis=1)
+        # clickhouse_client.client.insert_dataframe(
+        #     'INSERT INTO your_table_name VALUES', 
+        #     df, 
+        #     settings={'use_numpy': True}
+        # )
+        #
 
     load() >> build_features()
 
